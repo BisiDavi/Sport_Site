05a4a04fa5245854b030d46f3c82c09f9c9a45e5
Olubisi David <oludavidconnect@gmail.com>
2020-05-03 01:26:15 +0100

    .

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('sport-harvoxx.herokuapp.com'),
+            'url' => env('https://www.db4free.net/'),
             'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'sportdatabase'),
